4f5469c9bd3c907c8a7eb6c5233f68574d37c38e
SalmaQassem99 <salmaqassem49@gmail.com>
2024-03-24 12:53:05 +0200

    updated icons

--- JS/script.js
+++ JS/script.js
@@ -43,7 +43,15 @@ playButton.addEventListener("click", () => {
       document.querySelector(".game-background").textContent
     })`;
     body.classList.add("show");
+    pauseButton.style.visibility = "visible";
+    soundButton.style.display = "flex";
   });
+});
+pauseButton.addEventListener("click", () => {
+  const hiddenIcon = pauseButton.querySelector("i.hide");
+  const shownIcon = pauseButton.querySelector("i:not(.hide)");
+  hiddenIcon.classList.remove("hide");
+  shownIcon.classList.add("hide");
 });
 gameImages.forEach((image) => {
   image.addEventListener("click", () => {
